7e9a40720d1673a53c903b0c5eaedc720a2335e3
saces <saces@c-base.org>
2019-08-11 00:24:34 +0200

    add sources for go-olm

--- determinant.sh
+++ determinant.sh
@@ -7,3 +7,6 @@ docker-compose run -u $(id -u ${USER}):$(id -g ${USER}) olm-src
 
 docker-compose build olm-lib
 docker-compose run -u $(id -u ${USER}):$(id -g ${USER}) olm-lib
+
+docker-compose build go-olm-src
+docker-compose run -u $(id -u ${USER}):$(id -g ${USER}) go-olm-src
