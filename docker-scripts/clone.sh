6d9c02f611fc227ac39e4b6f361419ba758860dc
saces <saces@c-base.org>
2019-09-07 21:54:02 +0200

    get & patch matrix-doc

--- docker-scripts/clone.sh
+++ docker-scripts/clone.sh
@@ -6,8 +6,12 @@ LOCALREPO=/ext-src/$DET_LOCALSRC
 
 if [ ! -d $LOCALREPO ]
 then
-    git clone $DET_REPOSURL $LOCALREPO
+    git clone $DET_REPOS_PATCH_URL $LOCALREPO
+    cd $LOCALREPO
+    git config user.name "$DET_REPOS_USER_NAME"
+    git config user.email "$DET_REPOS_USER_EMAIL"
 else
     cd $LOCALREPO
-    git pull --rebase $DET_REPOSURL
+    git pull --rebase $DET_REPOS_PATCH_URL
 fi
+git pull --rebase $DET_REPOS_MATRIX_URL $DET_REPOS_MATRIX_BRANCH
